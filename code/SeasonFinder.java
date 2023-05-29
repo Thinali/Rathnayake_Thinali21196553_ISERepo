37bb0a007b6d887715a584569618117639e74929
Thinali <thinalirathnayake@gmail.com>
2023-05-29 14:47:26 +0530

    Black/White Box Tests + code corrections

--- code/SeasonFinder.java
+++ code/SeasonFinder.java
@@ -45,6 +45,7 @@ public class SeasonFinder {
             season = findSeason(country, month, noongarBit);
             if(season == "inexistent"){
                 System.out.println("Noongar Seasons do not apply to entered Country. Please try again.");
+                scanner.close();
                 return;
             }
         }
@@ -58,10 +59,12 @@ public class SeasonFinder {
         String season;
         if (country.equalsIgnoreCase("Australia")) {
             season = getSeasonInAustralia(month);
-        } else if (country.equalsIgnoreCase("Canada")) {
-            season = getSeasonInCanada(month);
-        } else if (country.equalsIgnoreCase("India")) {
-            season = getSeasonInIndia(month);
+        } else if (country.equalsIgnoreCase("Spain") || country.equalsIgnoreCase("Japan")) {
+            season = getSeasonInSpainJapan(month);
+        } else if (country.equalsIgnoreCase("Malaysia") || country.equalsIgnoreCase("Sri Lanka")) {
+            season = getSeasonInMalaysiaSriLanka(month);
+        } else if (country.equalsIgnoreCase("Mauritius")) {
+        season = getSeasonInMauritius(month);
         } else {
             season = "Unknown";
         }
@@ -76,6 +79,7 @@ public class SeasonFinder {
         } else {
             season = getNoongarSeason(month);
         }
+    }
     else{
         season = "inexistent";
     }
@@ -96,7 +100,7 @@ public class SeasonFinder {
         }
     }
 
-    public static String getSeasonInCanada(int month) {
+    public static String getSeasonInSpainJapan(int month) {
         if (month >= 3 && month <= 5) {
             return "Spring";
         } else if (month >= 6 && month <= 8) {
@@ -110,15 +114,29 @@ public class SeasonFinder {
         }
     }
 
-    public static String getSeasonInIndia(int month) {
-        if (month >= 3 && month <= 6) {
-            return "Summer";
-        } else if (month >= 7 && month <= 9) {
-            return "Monsoon";
-        } else if (month >= 10 && month <= 11) {
-            return "Post-Monsoon";
-        } else if (month >= 12 || (month >= 1 && month <= 2)) {
+    public static String getSeasonInMauritius(int month) {
+        if (month == 5) {
+            return "Autumn";
+        } else if (month >= 6 && month <= 9) {
             return "Winter";
+        } else if (month == 10) {
+            return "Spring";
+        } else if ((month >= 11 && month <= 12) || (month >= 1 && month <= 4)) {
+            return "Summer";
+        } else {
+            return "Unknown";
+        }
+    }
+
+    public static String getSeasonInMalaysiaSriLanka(int month) {
+        if (month >= 3 && month <= 4) {
+            return "Inter-monsoon";
+        } else if (month >= 5 && month <= 9) {
+            return "Southeast Monsoon";
+        } else if (month >= 10 && month <= 11) {
+            return "Inter-monsoon";
+        } else if (month >= 12 || (month >= 1 && month <= 2)) {
+            return "Northeast Monsoon";
         } else {
             return "Unknown";
         }
@@ -143,8 +161,8 @@ public class SeasonFinder {
     }
 
     public static boolean isValidCountry(String country) {
-        return country.equalsIgnoreCase("Australia") || country.equalsIgnoreCase("Canada")
-                || country.equalsIgnoreCase("India");
+        return country.equalsIgnoreCase("Australia") || country.equalsIgnoreCase("Malaysia") || country.equalsIgnoreCase("Sri Lanka")
+                || country.equalsIgnoreCase("Mauritius") || country.equalsIgnoreCase("Japan") || country.equalsIgnoreCase("Spain");
     }
 
     public static boolean isValidMonth(int month) {
